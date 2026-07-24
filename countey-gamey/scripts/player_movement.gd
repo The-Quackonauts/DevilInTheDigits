f4f761047885e722dadb37dd1e00a8d6b1131f18
plaguemarine <calumrl@outlook.com>
2026-07-24 10:58:10 +0100

    added gnome in game animations and floating mechanic

--- countey-gamey/scripts/player_movement.gd
+++ countey-gamey/scripts/player_movement.gd
@@ -3,18 +3,27 @@ extends CharacterBody2D
 const SPEED = 200.0
 const JUMP_VELOCITY = -567.0
 
-@onready var sprite: Sprite2D = $Sprite2D
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+var floating := false
+var can_float := false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
+	if floating:
+			velocity += get_gravity() * delta * 0.2
+	else:
 		velocity += get_gravity() * delta
 		
-	
-
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	# floating
+	if Input.is_action_just_released("jump") and not is_on_floor():
+		can_float = true
+	if Input.is_action_pressed("jump") and not is_on_floor() and can_float and velocity.y > 0:
+		floating = true
+	else:
+		floating = false
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("move_left", "move_right")
@@ -26,7 +35,16 @@ func _physics_process(delta: float) -> void:
 		sprite.flip_h = true
 		
 	# animation
-		
+	if is_on_floor():	
+		if direction == 0:
+			sprite.play("idle")
+		else:
+			sprite.play("run")
+	else:
+		if floating == true:
+			sprite.play("floating")	
+		else:
+			sprite.play("idle")
 	# move
 	if direction:
 		velocity.x = direction * SPEED
