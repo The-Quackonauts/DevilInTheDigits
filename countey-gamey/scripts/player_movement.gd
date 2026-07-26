b5d5562f3606e96988f5f2cd5d25302020826ce3
plaguemarine <calumrl@outlook.com>
2026-07-26 09:58:53 +0000

    added a jump noise

--- countey-gamey/scripts/player_movement.gd
+++ countey-gamey/scripts/player_movement.gd
@@ -4,6 +4,7 @@ const SPEED = 200.0
 const JUMP_VELOCITY = -567.0
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var jump_noise: AudioStreamPlayer2D = $JumpNoise
 var floating := false
 var can_float := false
 
@@ -19,6 +20,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_noise.play()
 	# floating
 	if Input.is_action_just_released("jump") and not is_on_floor():
 		can_float = true
